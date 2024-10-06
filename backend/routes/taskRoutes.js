818e6d864a0a886c0e171d8992fdc05b396de693
Arslan Kamchybekov <arslankamcybekov7@gmail.com>
2024-10-06 10:30:15 -0500

    getRandomTask

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getTasks, completeTask } = require('../services/taskService');
+const { getTasks, completeTask, getRandomTask } = require('../services/taskService');
 const router = express.Router();
 
 // Route to get all tasks
@@ -23,6 +23,18 @@ router.post('/:taskId', async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
+});
+
+// Get a random task from the list that is not already completed
+router.get('/random', async (req, res) => {
+    const { email } = req.body;
+    try {
+        const randomTask = await getRandomTask(email);
+        console.log(randomTask);
+        res.status(200).json(randomTask);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 });
 
 module.exports = router;
