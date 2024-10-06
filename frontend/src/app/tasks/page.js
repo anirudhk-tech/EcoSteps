b449d9fea4fa2980c3408f0e58eec0d449d49dab
Arslan Kamchybekov <arslankamcybekov7@gmail.com>
2024-10-06 10:36:03 -0500

    routes fixed

--- frontend/src/app/tasks/page.js
+++ frontend/src/app/tasks/page.js
@@ -39,7 +39,7 @@ export default function TasksPage() {
 
   const completeTask = async (id) => {
     try {
-      const response = await fetch(`http://localhost:4000/${id}`, {
+      const response = await fetch(`http://localhost:4000/tasks/${id}`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email: email })
